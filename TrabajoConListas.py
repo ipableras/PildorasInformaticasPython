66e09b669a8db4f51f6ee09966e15cab2bafbba1
Pablo <pablovillarey@gmail.com>
2021-06-04 13:40:58 +0200

    Lista pyrhon video 6

--- TrabajoConListas.py
+++ TrabajoConListas.py
@@ -1,3 +1,20 @@
 # Listas
+# doc python 
 
 trabajadores =["Ana", "María", "Antonio", "Miguel"]
+
+print(trabajadores)
+
+print(len(trabajadores))
+
+trabajadores.append("Juan")
+
+print(trabajadores)
+
+print(trabajadores[2])
+
+print(trabajadores)
+print(trabajadores[-2])
+print(trabajadores)
+
+

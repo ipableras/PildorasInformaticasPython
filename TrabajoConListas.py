367f6c941f0ca99215b5862f4c45192c07a2a816
Pablo <pablovillarey@gmail.com>
2021-06-13 12:58:44 +0200

    Commit de Tuplas y algo de Listas anteriores

--- TrabajoConListas.py
+++ TrabajoConListas.py
@@ -7,6 +7,8 @@ print(trabajadores)
 
 print(len(trabajadores))
 
+print(trabajadores.index("Antonio"))
+
 trabajadores.append("Juan")
 
 print(trabajadores)
@@ -17,4 +19,16 @@ print(trabajadores)
 print(trabajadores[-2])
 print(trabajadores)
 
+print()
+
+# Borrar Maria
+print(trabajadores)
+del trabajadores[1]
+print(trabajadores)
+
+print()
+
+print(trabajadores[0:3])
+print(trabajadores[2:3])
+print(trabajadores[3:2])
 
---
+++ TrabajoConTuplas.py
@@ -0,0 +1,54 @@
+# Tuplas
+# Son mas rapidas, pero son más rigidas
+
+misDatos = ("Juan", 13, 1, 2002)
+
+print(misDatos)
+
+misDatosLista=list(misDatos)
+print(misDatosLista)
+
+# Vamos a hacerlo alreves
+misDatos2 = ["Juan", 13, 1, 2002]
+
+misDatosTupla = tuple(misDatos2)
+print(misDatosTupla)
+
+print()
+
+# De esta forma otra vez
+misDatosTupla = list(misDatos2)
+print(misDatosTupla)
+misDatosTupla = tuple(misDatos2)
+print(misDatosTupla)
+
+print()
+
+# Vamos a ver que estamos mirando en la Tupla
+print(13 in misDatosTupla)
+print(25 in misDatosTupla)
+print("Juan" in misDatosTupla)
+
+print()
+
+# count()
+print(misDatosTupla.count("Juan"))
+print(misDatosTupla.count("Jua"))
+print(misDatosTupla.count(13))
+misDatosTupla = ("Juan", 13, 1, 2002, "Juan")
+print(misDatosTupla)
+# Ahora tenemos dos numeros en "Juan"
+print(misDatosTupla.count("Juan"))
+
+print()
+
+# len()
+print(len(misDatosTupla))
+
+print()
+
+# desempaquetado de tupla
+print(misDatos)
+nombre, dia, mes, agno=misDatos
+print(mes)
+

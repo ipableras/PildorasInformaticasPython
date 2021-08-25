a26fda8e590dbf9b2f584167be5d50673711ef91
Pablo <pablovillarey@gmail.com>
2021-08-25 18:45:05 +0200

    Commit con funciones

---
+++ TrabajoConFunciones.py
@@ -0,0 +1,10 @@
+# Funciones
+
+def imprimeMensajes():
+    print("Esto es un curso de Python")
+    print("El cursp empiezaa a comenzar")
+    print("Ponemos mas cosas")
+
+print("El lenguaje a terminado")
+
+imprimeMensajes()  # Lllamada/invocación de la función
